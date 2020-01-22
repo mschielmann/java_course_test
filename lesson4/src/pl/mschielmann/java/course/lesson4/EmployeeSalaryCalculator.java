bdf91235fd4a30c947e7534048b96f133541af3d
Michal Schielmann <michalschielmann@gmail.com>
2020-01-22 19:55:09 +0100

    lesson 4

---
+++ lesson4/src/pl/mschielmann/java/course/lesson4/EmployeeSalaryCalculator.java
@@ -0,0 +1,17 @@
+package pl.mschielmann.java.course.lesson4;
+
+public class EmployeeSalaryCalculator {
+
+    /*
+        1) Create an Employee class, that would allow you to create employee objects.
+        Employee class should have name, surname, position and salary fields.
+        2) Create an array of 5 employees, some with Junior, some with Senior positions.
+        3) Calculate the sum of salaries for all employees.
+        4) Calculate the of salaries for Junior position and for Senior salaries.
+        5) Print the results
+    */
+
+    public static void main(String[] args) {
+
+    }
+}
