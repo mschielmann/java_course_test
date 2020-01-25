a26a9d0751549f7bd7d4863dafcda195b38f91d2
Michal Schielmann <michalschielmann@gmail.com>
2020-01-25 13:01:29 +0100

    fix for fruit basket exercise

--- lesson4/src/pl/mschielmann/java/course/lesson4/exercise2/FruitBasketsApp.java
+++ lesson4/src/pl/mschielmann/java/course/lesson4/exercise2/FruitBasketsApp.java
@@ -11,33 +11,34 @@ public class FruitBasketsApp {
          * print the number of fruits and total weight of each type separately.
          * 3) Divide all fruits from the fruit basket into separate baskets, one for each type.
          * 4) As a double-check, print the number of fruits and their total weights for each basket separately.
+         * 5) Find the weight of the heaviest and of the lightest fruit in each basket.
          */
     }
 
     private static Fruit[] fruits = new Fruit[] {
-            new Fruit("Orange", 51),
-            new Fruit("Apple", 51),
+            new Fruit("Orange", 290),
+            new Fruit("Apple", 212),
             new Fruit("Plum", 51),
-            new Fruit("Pear", 51),
-            new Fruit("Apple", 51),
-            new Fruit("Plum", 51),
-            new Fruit("Banana", 51),
-            new Fruit("Orange", 51),
-            new Fruit("Apple", 51),
-            new Fruit("Plum", 51),
-            new Fruit("Plum", 51),
-            new Fruit("Pear", 51),
-            new Fruit("Pear", 51),
-            new Fruit("Banana", 51),
-            new Fruit("Banana", 51),
-            new Fruit("Orange", 51),
-            new Fruit("Banana", 51),
-            new Fruit("Apple", 51),
-            new Fruit("Orange", 51),
-            new Fruit("Apple", 51),
-            new Fruit("Pear", 51),
-            new Fruit("Pear", 51),
-            new Fruit("Plum", 51),
-            new Fruit("Plum", 51)
+            new Fruit("Pear", 122),
+            new Fruit("Apple", 199),
+            new Fruit("Plum", 55),
+            new Fruit("Banana", 190),
+            new Fruit("Orange", 288),
+            new Fruit("Apple", 209),
+            new Fruit("Plum", 57),
+            new Fruit("Plum", 52),
+            new Fruit("Pear", 119),
+            new Fruit("Pear", 126),
+            new Fruit("Banana", 210),
+            new Fruit("Banana", 205),
+            new Fruit("Orange", 310),
+            new Fruit("Banana", 198),
+            new Fruit("Apple", 203),
+            new Fruit("Orange", 303),
+            new Fruit("Apple", 195),
+            new Fruit("Pear", 121),
+            new Fruit("Pear", 119),
+            new Fruit("Plum", 52),
+            new Fruit("Plum", 54)
     };
 }
